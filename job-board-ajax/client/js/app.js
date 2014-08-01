d00fb196327d88946b920355dcc4a2a76d6459cf
Dan Johnson <dan@balancerockmedia.com>
2014-08-01 09:32:57 -0400

    edits

--- job-board-ajax/client/js/app.js
+++ job-board-ajax/client/js/app.js
@@ -1,11 +1,31 @@
 var jobBoard = {
     init: function() {
         $.getJSON('http://localhost:3000/jobs').done(function(response) {
-            $('#container').text(response.message);
+            
+            $.each(response.jobs, function() {
+                $('#posts').append('<h2>' + this.title + '</h2>');
+                $('#posts').append('<p>' + this.description + '</p>');
+            });
+            
+            $('#formStatus').hide();
+        });
+    },
+    
+    contactForm: function() {
+        $('form').on('submit', function(e) {
+            e.preventDefault();
+            
+            var params = $('form').serialize();
+            
+            $.post('http://localhost:3000/contact', params).done(function(response) {
+                $('#formContainer').html('<p>' + response.message + '</p>');
+            });
         });
     }
 };
 
 $(function() {
     jobBoard.init();
+    
+    jobBoard.contactForm();
 });
--- job-board-ajax/server/app.js
+++ job-board-ajax/server/app.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-// cross domain stuff
+app.use(express.bodyParser());
+
+// cross domain stuff (CORS)
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -14,9 +17,29 @@ app.all('*', function(req, res, next) {
 });
 
 // routes
+app.post('/contact', function(req, res) {
+    var params = req.body;
+    
+    // do something with params (e.g. send email, save to database)
+    
+    res.json(200, {
+        message: 'Thanks ' + params.name
+    });
+});
+
 app.get('/jobs', function(req, res) {
     res.json(200, {
-        message: 'Listing of jobs'
+        message: 'Listing of jobs',
+        jobs: [
+            {
+                title: 'Senior Software Engineer',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            {
+                title: 'Associate Software Engineer',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        ]
     });
 });
 
