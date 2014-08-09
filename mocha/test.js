0e8eee5062d5250420f4ba79cc15b4d569aa6427
Dan Johnson <dan@balancerockmedia.com>
2014-08-09 08:58:32 -0400

    edits

--- mocha/test.js
+++ mocha/test.js
@@ -10,4 +10,14 @@ describe('MathHelpers', function() {
             assert.strictEqual(MathHelpers.add(1,2), 3);
         });
     });
+    
+    describe('myFunction()', function() {
+        it('should return 6 when called with [1, 2, 3, 4, 5]', function() {
+            assert.strictEqual(MathHelpers.myFunction([1, 2, 3, 4, 5]), 6);
+        });
+        
+        it('should return 4 when called with [1, 5, 4]', function() {
+            assert.strictEqual(MathHelpers.myFunction([1, 5, 4]), 4);
+        });
+    });
 });
